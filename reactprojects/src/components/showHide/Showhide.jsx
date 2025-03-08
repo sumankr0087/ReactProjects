faa3152cf1aa292bca08a7411424577784de8333
sumankr0087 <suman.kumar@essentia.dev>
2025-03-08 07:11:55 +0530

    complete the show hide logic

--- reactprojects/src/components/showHide/Showhide.jsx
+++ reactprojects/src/components/showHide/Showhide.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Showhide() {
+  const [showHide, setShowHide] = useState(false);
+
+  const toggleShowHide = () => {
+    setShowHide((prev) => !prev);
+  };
+
   return (
     <div>
-      
+      <div className='bg-blue-400 text-center text-white p-4 rounded-lg w-64 min-w-[16rem]'>
+        <button
+          className='bg-green-600 p-2 rounded w-full'
+          onClick={toggleShowHide}
+        >
+          {showHide ? 'Hide' : 'Show'}
+        </button>
+        {!showHide && <div className='mt-2'>Welcome to the Show/Hide Demo!</div>}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Showhide
+export default Showhide;
